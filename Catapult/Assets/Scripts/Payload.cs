1d0f385ad1864ebae04a60bcccb5f1274b442ac3
Oisin Wilson <C00213826@itcarlow.ie>
2019-11-11 21:16:34 +0000

    push

---
+++ Catapult/Assets/Scripts/Payload.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Payload : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Rigidbody2D firePoint;
+    public float maxDistance = 2.0f;
+    public float releaseTime = 0.15f;
+    private bool isPressed = false;
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            
+            if (Vector3.Distance(firePoint.position, mousePos) > maxDistance)
+            {
+                rb.position = firePoint.position + ( mousePos - firePoint.position).normalized * maxDistance;
+            }
+            else
+            {
+                rb.position = mousePos;
+            }
+        }
+    }
+
+
+    void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
+
+        this.enabled = false;
+
+    }
+
+}
